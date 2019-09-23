37e4f9caf7ccbb69c202d73854c28aa52a7f90a7
vuhoangg120699 <vudlhgcd17142@fpt.edu.vn>
2019-09-23 11:01:13 +0700

    update polymorphism lab

---
+++ Polymorphism_Problem2/Polymorphism_Problem2/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Polymorphism_Problem2
+{
+    public abstract class Animals
+    {
+        protected Animals(string name, string favouriteFood)
+        {
+            this.Name = name;
+            this.FavouriteFood = favouriteFood;
+        }
+
+        public string Name { get; set; }
+
+        public string FavouriteFood { get; set; }
+
+        public virtual string ExplainMyself()
+        {
+            return $"I am {this.Name} and my favourite food is {this.FavouriteFood}";
+        }
+    }
+    public class Cat : Animals
+    {
+        public Cat(string name, string favouriteFood) : base(name, favouriteFood)
+        {
+        }
+        public override string ExplainMyself()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine(base.ExplainMyself())
+                .Append("MEEOW");
+            return sb.ToString();
+        }
+    }
+    public class Dog : Animals
+    {
+        public Dog(string name, string favouriteFood)
+        : base(name, favouriteFood)
+        {
+        }
+
+        public override string ExplainMyself()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine(base.ExplainMyself())
+                .Append("DJAAF");
+            return sb.ToString();
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Animals cat = new Cat("Pesho", "Whiskas");
+            Animals dog = new Dog("Gosho", "Meat");
+
+            Console.WriteLine(cat.ExplainMyself());
+            Console.WriteLine(dog.ExplainMyself());
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Polymorphism_Problem3/Polymorphism_Problem3/Program.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Polymorphism_Problem3
+{
+    public abstract class Shape
+    {
+        public abstract double CalculatePerimeter();
+
+        public abstract double CalculateArea();
+
+        public virtual string Draw()
+        {
+            return "Drawing";
+        }
+    }
+    public class Circle : Shape
+    {
+        private double radius;
+
+        public Circle(double radius)
+        {
+            Radius = radius;
+        }
+
+        public double Radius
+        {
+            get { return radius; }
+            private set { radius = value; }
+        }
+
+        public override double CalculateArea()
+        {
+            return Math.PI * Math.Pow(Radius, 2);
+        }
+
+        public override double CalculatePerimeter()
+        {
+            return 2 * Math.PI * Radius;
+        }
+
+        public override string Draw()
+        {
+            return base.Draw() + this.GetType().Name;
+        }
+    }
+    public class Rectangle : Shape
+    {
+        private double width;
+        private double height;
+
+        public Rectangle(double width, double height)
+        {
+            Width = width;
+            Height = height;
+        }
+
+        public double Width
+        {
+            get { return width; }
+            private set { width = value; }
+        }
+
+        public double Height
+        {
+            get { return height; }
+            private set { height = value; }
+        }
+
+        public override double CalculateArea()
+        {
+            return Width * Height;
+        }
+
+        public override double CalculatePerimeter()
+        {
+            return 2 * Width + 2 * Height;
+        }
+
+        public override string Draw()
+        {
+            return base.Draw() + this.GetType().Name;
+        }
+    }
+}
